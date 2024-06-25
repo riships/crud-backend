e8df883448072d66cf0077f613c2e2b473f64344
Rishi <rishi.edunext@gmail.com>
2024-06-25 17:48:18 +0530

    db

--- server.js
+++ server.js
@@ -7,15 +7,5 @@ const { connectDB } = require('./config/db');
 // Connect to the database
 connectDB();
 
-// Middleware
-// app.use(express.json());
-
-// Routes
-// app.use('/api/users', userRoutes);
-
-// Error handling middleware
-// app.use(require('./middlewares/errorMiddleware'));
-
-// Start the server
 const PORT = config.port || 5000;
 app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
