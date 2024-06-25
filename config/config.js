8b3b7b23fff84196656922fd5f87036d404aa3a9
Rishi <rishi.edunext@gmail.com>
2024-06-25 16:52:27 +0530

    started creating crud

---
+++ config/config.js
@@ -0,0 +1,10 @@
+require('dotenv').config(); // Load environment variables from a .env file into process.env
+
+const config = {
+    port: process.env.PORT || 5000, // Application port, default to 5000 if not set
+    dbURI: process.env.DB_URI || 'mongodb://localhost:27017/mydatabase', // Database URI, default to local MongoDB
+    jwtSecret: process.env.JWT_SECRET || 'your_jwt_secret', // Secret key for JWT authentication
+    env: process.env.NODE_ENV || 'development' // Application environment, default to 'development'
+};
+
+module.exports = config;
